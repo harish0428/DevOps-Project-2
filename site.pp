19306357443b96b59069d1f54c6055e6df689ed6
sateesh369 <sateeshdevops@gmail.com>
2016-10-27 00:29:45 -0400

    Create site.pp

---
+++ site.pp
@@ -0,0 +1,23 @@
+node 'project.puppetagent*.com' {  
+  class { 'apache':    
+    default_mods => false, 
+  }  
+
+  include apache::mod::ssl  
+
+  apache::vhost { 'project.puppetagent*.com-http':    
+    servername      => 'project.puppetagent*.com',    
+    port            => '80',    
+    docroot         => '/var/www/html/',    
+    redirect_status => 'permanent',    
+    redirect_dest   => 'https://project.puppetagent*.com/',  
+  }  
+
+  apache::vhost { 'project.puppetagent*.com-https':    
+    servername      => 'project.puppetagent*.com',    
+    port            => '443',    
+    docroot         => '/var/www/html/',    
+    ssl             => true,    
+  }
+}
+
